feea328145fc9b9819e739eab2df58a747a7edf3
Sarath Nair <sarath.nair@ck12.org>
2021-02-22 13:13:03 +0530

    Fixing few dependencies

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
-export const secret = 'd/7bGZrt?2-v@[vKsaf>3CqHNL*H/5*'; // Super secret key to use with JWT
-export const restCountryAPIKey = '84426a235a27138ed3e098e318ab7af0'; // https://fixer.io/ API key to get exchange rates
-export const apiLimitPerMinute = 60; // Number of requests allower per minute per user
+const secret = 'd/7bGZrt?2-v@[vKsaf>3CqHNL*H/5*'; // Super secret key to use with JWT
+const restCountryAPIKey = '84426a235a27138ed3e098e318ab7af0'; // https://fixer.io/ API key to get exchange rates
+const apiLimitPerMinute = 60; // Number of requests allower per minute per user
+
+module.exports = {
+    secret,
+    restCountryAPIKey,
+    apiLimitPerMinute
+}
